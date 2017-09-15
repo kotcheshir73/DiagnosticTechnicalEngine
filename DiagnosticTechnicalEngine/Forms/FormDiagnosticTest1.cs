680731cdec53b7383f746721167c4c0c7e8fff0b
Эгов Евгений <kotcheshir73@gmail.com>
2017-09-15 09:00:00 +0400

    Меняем форму

---
+++ DiagnosticTechnicalEngine/Forms/FormDiagnosticTest1.cs
@@ -0,0 +1,15 @@
+﻿using DiagnosticTechnicalEngine.StandartClasses;
+using ServicesModule.BindingModels;
+using ServicesModule.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnosticTechnicalEngine.Forms
+{
+	public class FormDiagnosticTest1 : StandartForm<DiagnosticTestViewModel, DiagnosticTestBindingModel>
+	{
+	}
+}
